ce9483b97a137f2b23f4a1afcbd4cd1d9055c9b7
462008783 <462008783@qq.com>
2018-03-22 10:13:26 +0800

    管理员登录

---
+++ backend/config/main.php
@@ -0,0 +1,70 @@
+<?php
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+return [
+
+    'id' => 'app-backend',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'backend\controllers',
+    'bootstrap' => ['log'],
+    'modules' => [],
+    'components' => [
+        //后台模版设置
+//        'view' => [
+//            'theme' => [
+//                'pathMap' => [
+//                    '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
+//                ],
+//            ],
+//        ],
+        'request' => [
+            'csrfParam' => '_csrf-backend',
+        ],
+        'user' => [
+            'identityClass' => 'backend\models\Admin',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+            'on beforeLogin' => function($event) {
+                $user = $event->identity; //这里的就是User Model的实例
+                $user->last_login_at = time();
+                $user->last_login_ip = Yii::$app->request->userIP;
+                $user->save();
+
+            },
+//            'on afterLogin' => function($event) {
+//                //the same
+//            }
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the backend
+            'name' => 'advanced-backend',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                ''=>'/admin/login/'
+            ],
+        ],
+
+    ],
+    'params' => $params,
+];
---
+++ common/config/main.php
@@ -0,0 +1,16 @@
+<?php
+return [
+
+    'language' => 'zh-CN',//中文设置
+    'timeZone' => 'Asia/Shanghai',//设置时区
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+    ],
+];
